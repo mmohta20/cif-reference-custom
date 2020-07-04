3563bfc38efff99290bce473a20a65319af318b4
mmohta <mmohta@adobe.com>
2020-07-04 14:50:54 +0530

    no message

--- src/common/ProductsLoader.js
+++ src/common/ProductsLoader.js
@@ -61,73 +61,13 @@ class ProductsLoader {
      * @returns {Promise} A Promise with the products data.
      */
     __searchProducts(params, actionParameters) {
-
         // This method returns a Promise, for example to simulate some HTTP REST call being performed
         // to the 3rd-party commerce system.
-
-        if (params.search || params.categoryId) { // Text search or fetching of the products of a category
-            return Promise.resolve({
-                total: 2,
-                offset: params.currentPage * params.pageSize,
-                limit: params.pageSize,
-                products: [
-                    {
-                        sku: 'product-1',
-                        title: 'Product #1',
-                        description: `Fetched product #1 from ${actionParameters.url}`,
-                        price: {
-                            currency: 'USD',
-                            amount: 12.34
-                        },
-                        categoryIds: [1, 2]
-                    },
-                    {
-                        sku: 'product-2',
-                        title: 'Product #2',
-                        description: `Fetched product #2 from ${actionParameters.url}`,
-                        price: {
-                            currency: 'USD',
-                            amount: 56.78
-                        },
-                        categoryIds: [2, 3]
-                    }
-                ]
-            });
-        } else if (params.filter && (params.filter.sku || params.filter.url_key || params.filter.category_id)) { // Get a product by sku or url_key
-            if ((params.filter.sku && params.filter.sku.eq) || (params.filter.url_key && params.filter.url_key.eq)) {
-                let key = params.filter.sku ? params.filter.sku.eq : params.filter.url_key.eq;
-                let filters = ['sku:'+key];
-                let options = {};
-                options.filter = filters;
-                options.sort=[];
-                options.q = actionParameters.query;
-                options.endPoint = actionParameters.magentoEndPoint;
-                return getProduct(options);
-            } else if (params.filter.sku && params.filter.sku.in) { // Get multiple products by skus
-                let filters = ['sku:'+params.filter.sku.in];
-                let options = {};
-                options.filter = filters;
-                options.sort=[];
-                options.q = actionParameters.query;
-                options.endPoint = actionParameters.magentoEndPoint;
-                return getProduct(options);
-            }
-            else if (params.filter.url_key && params.filter.url_key.in) { // Get multiple products by skus
-                let filters = ['url_key:'+params.filter.sku.in];
-                let options = {};
-                options.filter = filters;
-                options.sort=[];
-                options.q = actionParameters.query;
-                options.endPoint = actionParameters.magentoEndPoint;
-                return getProduct(options);
-            }
-            else if (params.filter.category_id) { // Get multiple products by skus
-                let options = {};
-                options.sort=[];
-                options.endPoint = actionParameters.magentoEndPoint;
-                options.q = actionParameters.query;
-                return getProduct(options);
-            }
+        if(actionParameters.magentoEndPoint && actionParameters.query)  { 
+            let options = {};
+            options.endPoint = actionParameters.magentoEndPoint;
+            options.q = actionParameters.query;
+            return getProduct(options);
         }
     }
 }
